f6a5f64e1eb8dee56574b12123b4a008ed75cf86
Jay Santarina <jay.santarina@gmail.com>
2023-03-12 18:11:40 +0100

    completed HTML and CSS, added and completed JS, added 404 page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 const btnGewicht = document.getElementById('gewichtBerekenen');
 const inputVeld = document.getElementById('gewichtGram');
 const outputVeld = document.getElementById('diamantGewichtCarat');
+
+function gewichtInCarat() {
+  let carat = inputVeld.value / 0.2;
+  outputVeld.classList.remove('hidden');
+  outputVeld.style.opacity = '0';
+  outputVeld.innerHTML = carat + ' Carats';
+  outputVeld.style.opacity = '1';
+}
