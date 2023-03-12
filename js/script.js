040ffa78cc31c4d61010f2468507ec2615373a14
Jay Santarina <jay.santarina@gmail.com>
2023-03-12 17:27:07 +0100

    started and completed HTML & CSS, started JS

---
+++ js/script.js
@@ -0,0 +1,4 @@
+'use strict';
+const btnGewicht = document.getElementById('gewichtBerekenen');
+const inputVeld = document.getElementById('gewichtGram');
+const outputVeld = document.getElementById('diamantGewichtCarat');
